cd1c3c3c9bde61928df70df6e97e9c89e0f9c9b8
metaland-vn <hxuanquang@gmail.com>
2025-02-21 14:17:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.154/build/three.module.js';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
 
